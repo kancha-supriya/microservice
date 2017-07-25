ae65f4be9c80e00eab53bf9bab00934fed138d7b
supriya kanchan <kanchansupriya22@gmail.com>
2017-07-25 12:51:42 +0530

    SP-1 Added .env.shadow file and updated config.py to add new connection to RDS

--- timed_release/config.py
+++ timed_release/config.py
@@ -4,6 +4,8 @@ import logging
 import os
 
 import newrelic.agent
+from sqlalchemy.pool import NullPool
+from sqlalchemy.pool import StaticPool
 
 
 # Service information
@@ -14,6 +16,7 @@ SERVICE_VERSION = '1.0.0'
 PROD_ENVIRONMENT = 'prod'
 DEV_ENVIRONMENT = 'dev'
 QA_ENVIRONMENT = 'qa'
+TEST_ENVIRONMENT = 'test'
 ENVIRONMENT = os.environ.get('Environment', DEV_ENVIRONMENT)
 
 if ENVIRONMENT == PROD_ENVIRONMENT:
@@ -27,3 +30,22 @@ LOGGER_NAME = 'ows1'
 
 # Generic handlers
 HEALTH_CHECK = '/hello/'
+
+# Database credentials
+RDS_DB_CONNECTION_PROPERTIES = {
+    'database': os.environ.get('RDS_MYSQL_DATABASE'),
+    'host': os.environ.get('RDS_MYSQL_HOST'),
+    'password': os.environ.get('RDS_MYSQL_PASSWORD'),
+    'port': os.environ.get('RDS_MYSQL_PORT'),
+    'user': os.environ.get('RDS_MYSQL_USER')
+}
+
+# Database config
+if ENVIRONMENT == TEST_ENVIRONMENT:
+    DB_URL = 'sqlite://'
+    POOL_CLASS = StaticPool
+else:
+    RDS_DB_URL = (
+        'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'
+        .format(**RDS_DB_CONNECTION_PROPERTIES))
+POOL_CLASS = NullPool
