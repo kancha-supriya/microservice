feb6a1f12fc29ab69d1f9f6db43e09e8c0436245
omkar-kshirsagar-sts <omkar.kshirsagar@sts.in>
2017-08-08 15:38:51 +0530

    error messages for validation, validation.

--- timed_release/constants/error.py
+++ timed_release/constants/error.py
@@ -3,10 +3,17 @@
 ERROR_CODE_AUTHORIZATION = 'authorization_error'
 ERROR_CODE_BAD_GRASS_REQUEST = 'bad_grass_request_error'
 ERROR_CODE_NOT_FOUND = 'not_found'
+ERROR_CODE_BAD_REQUEST = 'bad_request'
 
 ERROR_MESSAGE_MISSING_GRASS_HEADERS = 'Missing Grass Headers'
 ERROR_MESSAGE_INCOMPLETE_GRASS_HEADERS = 'Incomplete Grass Headers'
 ERROR_MESSAGE_INVALID_GRASS_ACCOUNT_TYPE = 'Invalid Grass Account Type'
 ERROR_MESSAGE_FORBIDDEN_USER = 'User is forbidden'
+ERROR_MESSAGE_TIME_OF_DAY_RELEASE = \
+    'Time of day Product should be in valid HH:MM:SS format'
+ERROR_MESSAGE_TIME_ZONE = 'Time Zone should be GMT or local'
+ERROR_MESSAGE_INTEGER_NON_NEGATIVE = \
+    '{} must be integer and can not be negative'
+ERROR_MESSAGE_REQUIRED_FIELDS = '{} fields are required'
 
 SUCCESS_CODE = 'ok'
